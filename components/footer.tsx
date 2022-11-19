d9fd11df82846ff85de4bc6636f5810956d289db
grizz12 <raka_088@smkassalaambandung.sch.id>
2022-11-19 17:53:55 +0700

    add footer.tsx

---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+import styles from './layout.module.css'
+
+export default function Footer() {
+    return (
+        <div>
+            <footer className={styles.footer}>
+                <p>Simple Blog By RakaTrust</p>
+            </footer>
+        </div>
+    )
+}

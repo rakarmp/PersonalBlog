2b06a0a79dd29f56c4575c5b4b573c3e04881de3
grizz12 <raka_088@smkassalaambandung.sch.id>
2022-11-19 17:30:23 +0700

    Change font roboto

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import "../styles/global.css";
 import { AppProps } from "next/app";
 import { useEffect, useState } from "react";
+import { Roboto } from "@next/font/google";
 import Loading from '../components/loading';
+
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   const [loading, setLoading] = useState(true)
@@ -13,7 +19,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
     {!loading ? (
-      <Component {...pageProps} />
+      <main className={roboto.className}>
+        <Component {...pageProps} />
+      </main>
     ): (
       <Loading />
     )}

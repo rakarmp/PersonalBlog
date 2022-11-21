21f1c222dae7ca685f10299fe5addc5cc9202765
grizz12 <raka_088@smkassalaambandung.sch.id>
2022-11-21 21:33:58 +0700

    test loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,29 +2,29 @@ import "../styles/global.css";
 import { AppProps } from "next/app";
 import { useEffect, useState } from "react";
 import { Roboto } from "@next/font/google";
-import Loading from '../components/loading';
+import Loading from "../components/loading";
 
 const roboto = Roboto({
-  weight: '400',
-  subsets: ['latin'],
-})
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
 
-  useEffect(function(){
-    setLoading(false)
-  }, [])
+  useEffect(function () {
+    setLoading(false);
+  }, []);
 
   return (
     <>
-    {!loading ? (
-      <main className={roboto.className}>
-        <Component {...pageProps} />
-      </main>
-    ): (
-      <Loading />
-    )}
+      {!loading ? (
+        <main className={roboto.className}>
+          <Component {...pageProps} />
+        </main>
+      ) : (
+        <Loading />
+      )}
     </>
-  )
+  );
 }
